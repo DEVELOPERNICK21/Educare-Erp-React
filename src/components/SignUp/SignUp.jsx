850678eff63bb7e290aca95b14c3d0044dab7812
nick <nikhilkubde21@gmail.com>
2021-05-14 04:46:06 +0530

    Finally added the backend conneccted to the frontend and storing the value in database

--- src/components/SignUp/SignUp.jsx
+++ src/components/SignUp/SignUp.jsx
@@ -1,7 +1,7 @@
 import React, { useState } from "react";
 import Button from "@material-ui/core/Button";
 import DoneAllIcon from "@material-ui/icons/DoneAll";
-import { Link } from "react-router-dom";
+import { Link,useHistory } from "react-router-dom";
 import './Signup.css';
 import anim from './Images/Blogging.json';
 
@@ -9,6 +9,57 @@ import anim from './Images/Blogging.json';
 
 
 const SignUp = () => {
+    const history = useHistory();
+    const [user, setUser] = useState(
+        {
+            name: "", email: "", phone: "", password: "", cpassword: ""
+        }
+    );
+
+    let name, value;
+
+    const handelInputs = (e) =>
+    {
+        name = e.target.name;
+        value = e.target.value;
+
+        setUser({...user, [name]:value })
+
+
+    }
+
+
+    const PostData = async (e) =>
+    {
+        e.preventDefault();
+
+    const {name, email, phone, password, cpassword} = user;
+
+    
+    const res = await fetch("/register",
+    {
+        method: "POST",
+        headers: {
+            "Content-Type" : "application/json"
+        },
+        body: JSON.stringify({
+            name, email, phone,password, cpassword
+        })
+    })
+
+    const data = await res.json();
+
+    if(data.status === 422 || !data ){
+        window.alert("invalid Registraion");
+        console.log("invalid Registraion");
+    }else{
+        window.alert("Successfully Registraion");
+        console.log("Successfully Registraion");
+
+        history.push("/login")
+    }
+    }
+
   
   
 
@@ -19,25 +70,25 @@ const SignUp = () => {
                     <div className='Headeline'>
                         <h1>Welcome, We make Schooling Easier</h1>
                     </div>
-                    <form>
+                    <form method="POST">
                         <div className='signupForm'>
                             <div className='signup'>
                                 <div className='form_group'>
-                                    <input type='text' placeholder='Full Name' required='' />
+                                    <input type='text' placeholder='Full Name' required='' value={user.name} onChange={handelInputs} name="name" />
                                 </div>
                                 <div className='form_group'>
-                                    <input type='email' placeholder='Email' required='' />
+                                    <input type='email' placeholder='Email' required='' value={user.email} onChange={handelInputs} name="email" />
                                 </div>
                                 <div className='form_group'>
-                                    <input type='text' placeholder='Phone Number' required='' />
+                                    <input type='text' placeholder='Phone Number' required='' value={user.phone} onChange={handelInputs} name="phone" />
                                 </div>
                                 <div className='form_group'>
-                                    <input type='password' placeholder='Password' required='' />
+                                    <input type='password' placeholder='Password' required='' value={user.password} onChange={handelInputs} name="password" />
                                 </div>
                                 <div className='form_group'>
-                                    <input type='password' placeholder='Confirm Password' required='' />
+                                    <input type='password' placeholder='Confirm Password' required='' value={user.cpassword} onChange={handelInputs} name="cpassword" />
                                 </div>
-                                <Button variant='contained' className="btn" color='disable'>
+                                <Button variant='contained' value="register" name="signup" onClick={PostData} className="btn" color='disable'>
                                     Sign Up
                                 </Button>
                                 <div className='account_exit'>
